d67850865887811c9003fd3c47a9da1a7960531b
thevckit <thevckit@icloud.com>
2020-12-11 01:22:42 +0300

    11/12/2020

--- thevckit/WPF/WPF-0/Properties/Settings.Designer.cs
+++ thevckit/WPF/WPF-0/Properties/Settings.Designer.cs
@@ -8,21 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace WPF_0.Properties
-{
+namespace WPF_0.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute( "Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0" )]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
-        private static Settings defaultInstance = ( (Settings)( global::System.Configuration.ApplicationSettingsBase.Synchronized( new Settings() ) ) );
-
-        public static Settings Default
-        {
-            get
-            {
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string EventTask {
+            get {
+                return ((string)(this["EventTask"]));
+            }
+            set {
+                this["EventTask"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1980-01-01")]
+        public global::System.DateTime Ban {
+            get {
+                return ((global::System.DateTime)(this["Ban"]));
+            }
+            set {
+                this["Ban"] = value;
             }
         }
     }
--- thevckit/WPF/WPF-0/View/Pages/LoginPage.xaml.cs
+++ thevckit/WPF/WPF-0/View/Pages/LoginPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WPF_0.View.Pages
 {
@@ -20,9 +21,81 @@ namespace WPF_0.View.Pages
     /// </summary>
     public partial class LoginPage : Page
     {
+        private static int errorCount = 3;
+        private DispatcherTimer BlockTimer = new DispatcherTimer();
         public LoginPage( )
         {
             InitializeComponent();
+            // Check Login
+            if (Properties.Settings.Default.EventTask != string.Empty)
+                txb_Username.Text = Properties.Settings.Default.EventTask;
+            // Event fot BanTimer
+            BlockTimer.Tick += BlockTimer_Tick;
+            // Check BanTimer
+            if (( DateTime.Now - Properties.Settings.Default.Ban ).Ticks < new TimeSpan( 0, 0, 10 ).Ticks)
+            {
+                this.IsEnabled = false;
+                BlockTimer.Start();
+            }
+
+        }
+
+        // Event Ban Timer
+        private void BlockTimer_Tick( object sender, EventArgs e )
+        {
+            if (( DateTime.Now - Properties.Settings.Default.Ban ).Ticks > new TimeSpan( 0, 0, 10 ).Ticks)
+            {
+                this.IsEnabled = true;
+                errorCount = 0;
+                BlockTimer.Stop();
+            }
+
+        }
+
+        // Rember me
+        public void RemberLoginUser( )
+        {
+            if (checkedRemeber.IsChecked == true)
+            {
+                Properties.Settings.Default.EventTask = txb_Username.Text;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Properties.Settings.Default.EventTask = "";
+                Properties.Settings.Default.Save();
+            }
+        }
+        private void btn_Cancel_Click( object sender, RoutedEventArgs e )
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btn_Login_Click( object sender, RoutedEventArgs e )
+        {
+            if (txb_Username.Text == "thevckit" & psb_Password.Password == "1234")
+            {
+                MessageBox.Show( "Success!" );
+                RemberLoginUser();
+            }
+            else
+            {
+                errorCount--;
+                if (errorCount == 0)
+                {
+                    MessageBox.Show( $"Authorization error, you have {errorCount} attempts left. You are locked on 10 seconds.", "Error!",
+                        MessageBoxButton.OK, MessageBoxImage.Error );
+                    this.IsEnabled = false;
+                    Properties.Settings.Default.Ban = DateTime.Now;
+                    Properties.Settings.Default.Save();
+                    BlockTimer.Start();
+                }
+                else
+                {
+                    MessageBox.Show( $"Authorization error, you have {errorCount} attempts left", "Error!",
+                        MessageBoxButton.OK, MessageBoxImage.Error );
+                }
+            }
         }
     }
 }
