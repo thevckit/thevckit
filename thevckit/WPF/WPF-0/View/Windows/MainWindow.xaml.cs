c291fe3b09bb4bf32cb8292d0efeb98574be8a7a
thevckit <thevckit@icloud.com>
2020-12-12 00:05:21 +0300

    12/12/2020

--- thevckit/WPF/WPF-0/View/Windows/MainWindow.xaml.cs
+++ thevckit/WPF/WPF-0/View/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using WPF_0.View.Pages;
 
 namespace WPF_0
@@ -24,7 +25,18 @@ namespace WPF_0
         public MainWindow( )
         {
             InitializeComponent();
+            // Now time
+            DispatcherTimer LiveTime = new DispatcherTimer();
+            LiveTime.Interval = TimeSpan.FromSeconds( 1 );
+            LiveTime.Tick += tick_Tick;
+            LiveTime.Start();
+            // Frame page Login
             frmObject.Navigate( new LoginPage() );
+        }
+
+        private void tick_Tick( object sender, EventArgs e )
+        {
+            txb_Time.Text = DateTime.Now.ToLongTimeString();
         }
     }
 }
