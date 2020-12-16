e94aca42a60aaef88184d73d6e496afad902d25e
thevckit <thevckit@icloud.com>
2020-12-16 16:05:12 +0300

    16/12/2020

---
+++ thevckit/WPF/WPF-1/Views/Pages/FontSize.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_1.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for FontSize.xaml
+    /// </summary>
+    public partial class FontSize : Page
+    {
+        public FontSize()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ thevckit/WPF/WPF-1/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WPF_1.Views.Pages;
+
+namespace WPF_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.Navigate(new FontSize());
+        }
+    }
+}
